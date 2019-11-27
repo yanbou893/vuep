e5f9330954790ef925892e421d4f81307ffe1562
EC2 Default User <ec2-user@ip-172-31-34-219.ap-northeast-1.compute.internal>
2019-11-27 07:04:24 +0000

    secound

--- src/main.js
+++ src/main.js
@@ -1,35 +1,8 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router';          // 追記 
 import App from './App.vue'
-import Main from './components/Main.vue';    // 追記
-import Quiz from './components/Quiz.vue';     // 追記
-import Result from './components/Result.vue'; // 追記
- 
-// ルータ設定 追記
-Vue.use(VueRouter);
-const router = new VueRouter({
-  routes: [
-    {
-      path: '/main',
-      component: Main
-    },
-    {
-      path: '/quiz',
-      component: Quiz
-    },
-    {
-      path: '/result',
-      component: Result
-    },
-    {
-      path: '*',
-      redirect: '/main'
-    }
-  ]
-});
- 
+
+Vue.config.productionTip = false
+
 new Vue({
-  el: '#app',
-  router, // 追記
-  render: h => h(App)
-});
+  render: h => h(App),
+}).$mount('#app')
